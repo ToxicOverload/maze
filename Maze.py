a0e3fd9afe4dcc6fbe9aefbcbd89b19759c4bd3e
Aiden Woodruff <aiden.woodruff@gmail.com>
2018-04-16 12:20:54 -0500

    Added Maze as a class
    Signed-off-by: Aiden Woodruff <aiden.woodruff@gmail.com>

--- Maze.py
+++ Maze.py
@@ -1,5 +1,5 @@
 import os
-import re
+import pygame
 
 # Synonyms for yes
 yes_synonyms = ["y", "yes", "sure", "okay", "fine", "affirmative", "all right", "very well", "of course", "by all means", "certainly", "absolutely", "indeed", "right", "agreed", "roger", "ok", "yeah", "yep", "yup", "okey-dokey", "yea", "aye"]
@@ -12,48 +12,78 @@ green = (0,255,0)
 gray = (100,100,100)
 purple = (255,0,255)
 
-# Import a maze from a file
-def import_maze (filename = None):
-
-    # Init variable
+class Maze:
     maze = []
-
-    if not os.path.exists("Mazes"):
-        os.makedirs("Mazes")
-    os.chdir("Mazes")
-
-    valid_mazes = [i.name for i in list(os.scandir()) if i.is_file()]
-
-    # Ask filename
-    while filename not in valid_mazes:
-        for i in valid_mazes:
-            print(i)
-        filename = input("File name: ")
-
-    # Open the file
-    with open(filename, "r") as file:
+    def __init__ (self, width=None, height=None):
+        self.maze = []
+        self.width = width
+        self.height = height
+        if width != None and height != None:
+            for y in range(height):
+                self.maze.append(list())
+                for x in range(width):
+                    self.maze[y].append("")
+    def import_file (self, filename = None):
+        """Import maze from a file"""
+        if not os.path.exists("Mazes"):
+            os.makedirs("Mazes")
+        valid_mazes = [i.name for i in list(os.scandir("Mazes")) if i.is_file()]
+        if len(valid_mazes) == 0:
+            return False
+        self.maze = []
+        # Ask filename
+        while filename not in valid_mazes:
+            for i in valid_mazes:
+                print(i)
+            filename = input("File name: ")
+        # Open the file
+        with open(os.path.join("Mazes", filename), "r") as file:
             # Read the file as lines
             lines = file.readlines()
-
-            # Remove header line
-            lines.pop(0)
-
-            # Set maze to the body lines and remove the \n at the end of each line
+            # Read in dimensions
+            self.width = int(lines[0].strip("\n").split("x")[0])
+            self.height = int(lines[0].strip("\n").split("x")[1])
+            # Set maze to the body lines and remove the strip lines
             for line in lines:
-                line = line.strip("\n")
-                row = line.split(",")
-                maze.append(row)
-    return maze
-
-def save_maze(board):
-    save_name = ""
-    while len(save_name) == 0:
-        save_name = input("Save file name: ")
-    os.makedirs("Mazes", exist_ok=True)
-    os.chdir("Mazes")
-
-    with open(save_name, "w") as f:
-        print("width: ", len(board), file=f)
-        for row in board:
-            print(str(row).translate(str.maketrans("","","[]' ")), file=f)
-    return True
+                self.maze.append(list(line.strip("\n")))
+        return True
+    def save (self, filename = None):
+        """Save maze to file"""
+        while len(filename) == 0:
+            filename = input("Save file name: ")
+        if not os.path.exists("Mazes"):
+            os.makedirs("Mazes")
+        with open(os.path.join("Mazes", filename), "w") as f:
+            print(self.width, "x", self.height, sep="", file=f)
+            for row in self.maze:
+                for cell in row:
+                    print(cell, file=f, end="")
+                print("", file=f)
+        return True
+    def at (self, x, y):
+        """Return value in maze"""
+        if x < self.width and y < self.height:
+            return self.maze[y][x]
+        else:
+            return False
+    def set (self, x, y, value):
+        """Set a value to maze"""
+        if x < self.width and y < self.height:
+            self.maze[y][x] = value
+            return True
+        else:
+            return False
+    def draw (self, screen, tile_size):
+        if not type(screen) is pygame.Surface:
+            return False
+        screen.fill(white)
+        for y in range(self.height):
+            for x in range(self.width):
+                if self.at(x, y) == ".":
+                    pygame.draw.rect(screen, black, [x * tile_size, y * tile_size, tile_size, tile_size])
+                elif self.at(x, y) == "@":
+                    pygame.draw.rect(screen, blue, [x * tile_size, y * tile_size, tile_size, tile_size])
+                elif self.at(x, y) == "&":
+                    pygame.draw.rect(screen, purple, [x * tile_size, y * tile_size, tile_size, tile_size])
+        pygame.display.update()
+        return True
--- Snakey Mazes.py
+++ Snakey Mazes.py
@@ -2,12 +2,11 @@ import pygame
 from random import randint, choice
 import os
 from math import sqrt
+import Maze
 
 wait = False
 
 # . is walls, # is nothing, @ is finish, & is start
-maze = []
-
 maze_size = 0
 while not (maze_size >= 5):
     maze_size = input("How big do you want it to be? ")
@@ -15,128 +14,89 @@ while not (maze_size >= 5):
         maze_size = int(maze_size)
         if not maze_size >= 5:
             print("Please make it at least 5!")
-            pass
     except ValueError:
         print("Please make it an integer.")
         maze_size = 0
 
-for y in range(maze_size):
-    row = []
-    for x in range(maze_size):
-        row.append(".")
-    maze.append(row)
-
+maze = Maze.Maze(maze_size, maze_size)
 cells_list = []
-
-white = (255,255,255)
-black = (0,0,0)
-blue = (0,0,255)
-green = (0,255,0)
-purple = (255,0,255)
 
 players = []
 creating = True
 events = []
 
+tile_size = int(600 / maze.height)
+pygame.init()
+size = [maze.width * tile_size, maze.height * tile_size]
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Maze Fun!")
+
 wait_time = 0
-
-path = os.path.dirname(__file__)
-
-if not os.path.exists(os.path.join(path, "mazes")):
-    os.makedirs(os.path.join(path, "mazes"))
-
-def save_maze(board):
-    save_name = ""
-
-    while len(save_name) == 0:
-        save_name = input("What do you want to save it as? (Don't include the extension) ")
-
-    save_name = os.path.join(path, "mazes", save_name + ".txt")
-
-    file = open(save_name, "w+")
-
-    file.write("width: " + str(maze_size) + "\n")
-
-    temp_board = board
-    for row in temp_board:
-        row = str(row)
-        row = row.replace("[","")
-        row = row.replace("]","")
-        row = row.replace("'","")
-        row = row.replace(" ","")
-        file.write(row + "\n")
 
 def neighbors(cell):
     neighbors = []
     x = cell[0]
     y = cell[1]
 
-    if x-2 >= 0 and maze[y][x-2] == ".":
+    if x-2 >= 0 and maze.at(x-2, y) == ".":
         neighbors.append([x-2,y])
-    if x+2 <= maze_size - 1 and maze[y][x+2] == ".":
+    if x+2 <= maze.width - 1 and maze.at(x+2, y) == ".":
         neighbors.append([x+2,y])
-    if y-2 >= 0 and maze[y-2][x] == ".":
+    if y-2 >= 0 and maze.at(x, y-2) == ".":
         neighbors.append([x,y-2])
-    if y+2 <= maze_size - 1 and maze[y+2][x] == ".":
+    if y+2 <= maze_size - 1 and maze.at(x, y+2) == ".":
         neighbors.append([x,y+2])
     return neighbors
 
 def create_passage(cell, cell2):
     if cell[0] > cell2[0]:
-        maze[cell[1]][cell[0]-1] = "#"
+        maze.set(cell[0]-1, cell[1], "#")
     if cell[0] < cell2[0]:
-        maze[cell[1]][cell[0]+1] = "#"
+        maze.set(cell[0]+1, cell[1], "#")
     if cell[1] > cell2[1]:
-        maze[cell[1]-1][cell[0]] = "#"
+        maze.set(cell[0], cell[1]-1, "#")
     if cell[1] < cell2[1]:
-        maze[cell[1]+1][cell[0]] = "#"
+        maze.set(cell[0], cell[1]+1, "#")
 
 def set_start_and_end():
-    x = randint(0,maze_size-1)
-    y = randint(0,maze_size-1)
+    x = randint(0, maze.width - 1)
+    y = randint(0, maze.height - 1)
 
-    x2 = randint(0,maze_size-1)
-    y2 = randint(0,maze_size-1)
+    x2 = randint(0, maze.width-1)
+    y2 = randint(0, maze.height-1)
 
-    while not (sqrt((x2-x)**2 + (y2-y)**2) > maze_size/2 * 1.5 and maze[y][x] == "#" and maze[y2][x2] == "#"):
-        x = randint(0,maze_size-1)
-        y = randint(0,maze_size-1)
+    while not (sqrt((x2-x)**2 + (y2-y)**2) > maze_size/2 * 1.5 and maze.at(x,y) == "#" and maze.at(x2,y2) == "#"):
+        x = randint(0, maze.width - 1)
+        y = randint(0, maze.height - 1)
         
-        x2 = randint(0,maze_size-1)
-        y2 = randint(0,maze_size-1)
+        x2 = randint(0, maze.width - 1)
+        y2 = randint(0, maze.height - 1)
 
-    maze[y][x] = "@"
-    maze[y2][x2] = "&"
+    maze.set(x, y, "@")
+    maze.set(x2, x2, "&")
 
 def create():
     global cells_list
     global creating
-
-    start_cell = [-1,-1]
+    start_cell = [-1, -1]
     while start_cell[0] % 2 == 1 and start_cell[0] < 0 and start_cell[1] % 2 == 1 and start_cell[1] < 0:
-        start_cell = [randint(1,maze_size-2),randint(1,maze_size-2)]
-
-    cells_list.append([start_cell[0],start_cell[1]])
-    maze[cells_list[0][1]][cells_list[0][0]] = "#"
+        start_cell = [randint(1, maze.width - 2), randint(1, maze.height - 2)]
+    cells_list.append([start_cell[0], start_cell[1]])
+    maze.set(cells_list[0][0], cells_list[0][1], "#")
 
     while len(cells_list) > 0:
-        current_cell = cells_list[len(cells_list)-1]
-
+        current_cell = cells_list[len(cells_list) - 1]
         if len(neighbors(current_cell)) > 0:
             random_neighbor = choice(neighbors(current_cell))
-            maze[random_neighbor[1]][random_neighbor[0]] = "#"
-
+            maze.set(random_neighbor[0], random_neighbor[1], "#")
             create_passage(current_cell, random_neighbor)
-
             cells_list.append(random_neighbor)
         else:
             cells_list.remove(current_cell)
-
-        
         get_events()
         handle_events(events)
         if wait:
-            draw(maze)
+            maze.draw(screen, tile_size)
             pygame.time.wait(wait_time)
     set_start_and_end()
 
@@ -145,7 +105,6 @@ def get_events():
     events = []
     for event in pygame.event.get():
         events.append(event)
-
 def handle_events(events_list):
     global playing
     for event in events_list:
@@ -158,34 +117,12 @@ def handle_events(events_list):
                 pygame.quit()
                 quit()
             elif event.key == pygame.K_s:
-                save_maze(maze)
-
-def draw(board):
-    screen.fill(white)
-    for x in range(len(board)):
-        for y in range(len(board[x])):
-            if board[x][y] == ".":
-                pygame.draw.rect(screen, black, [y*tile_size,x*tile_size,tile_size,tile_size])
-            elif board[x][y] == "@":
-                pygame.draw.rect(screen, blue, [y*tile_size,x*tile_size,tile_size,tile_size])
-            elif board[x][y] == "&":
-                pygame.draw.rect(screen, purple, [y*tile_size,x*tile_size,tile_size,tile_size])
-    pygame.display.update()
-
-tile_size = int(600/len(maze))
-
-pygame.init()
-size = [len(maze)*tile_size,len(maze[0])*tile_size]
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption("Maze Fun!")
-
+                maze.save()
 create()
-
 while True:
     get_events()
     handle_events(events)
-    draw(maze)
+    maze.draw(screen, tile_size)
     pygame.time.wait(wait_time)
-
 pygame.quit()
 quit()
